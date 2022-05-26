67e10168f6b6b5881ffc0913b8bb00929f1fc5df
Olivia McSweeney <mcsweeno@gmail.com>
2022-05-26 16:46:34 +0100

    coded app

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
+  resources :lists do
+    resources :bookmarks, only: [:new, :create, :show]
+  end
+    resources :bookmarks, only: [:destroy]
 end
